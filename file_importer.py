d67dd5f7c274821342695b2d7bcc04b00e9d5be0
Owen-Shadburne <111443367+Owen-Shadburne@users.noreply.github.com>
2022-10-24 09:00:29 -0500

    Update file_importer.py

--- file_importer.py
+++ file_importer.py
@@ -1,22 +1,58 @@
-import sqlite3
+import sqlite3, csv, json, xml
 
-table = sqlite3.connect('data.sqlite')
+data = sqlite3.connect('data.sqlite')
 
 class Importer:
 
     def import_file(self, filename):
-        print("BUTTS")
-        if(filename[:-3] == "csv"):
-            self.import_csv(filename)
-        elif(filename[:-4] == "json"):
-            self.import_json(filename)
-        elif(filename[:-3] == "xml"):
-            self.import_xml(filename)
-        #table.execute("DROP TABLE IF EXISTS data")
-        #table.execute("""CREATE TABLE IF NOT EXISTS data ()""")
+        if(filename[-3:] == "csv"):
+            self.import_csv(filename[:-4])
+        elif(filename[-4:] == "json"):
+            self.import_json(filename[:-5])
+        elif(filename[-3:] == "xml"):
+            self.import_xml(filename[:-4])
 
     def import_csv(self, filename):
-        print("THIS IS A CSV")
+        data.execute("DROP TABLE IF EXISTS " + filename)
+
+        results = []
+        creation = "id INTEGER PRIMARY KEY, "
+        insertHeader = ""
+        with open((filename + '.csv'), mode='r', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                for title in row:
+                    item = row[title]
+                    insertHeader += title + ", "
+                    if len(item) < 6 and item.isdigit():
+                        results.append((title, "INTEGER"))
+                        creation += title + " INTEGER, "
+                    elif "." in item and item.replace(".", "").isdigit():
+                        results.append((title, "REAL"))
+                        creation += title + " REAL, "
+                    else:
+                        results.append((title, "TEXT"))
+                        creation += title + " TEXT, "
+                break
+
+        insertHeader = insertHeader[-2]
+        print(results)
+        data.execute("CREATE TABLE IF NOT EXISTS " + filename + "(" + creation[:-2] + ")")
+
+        with open((filename + '.csv'), mode='r', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                values = ""
+                for title in row:
+                    item = row[title]
+                    if len(item) < 6 and item.isdigit():
+                        results.append((title, "INTEGER"))
+
+                    elif "." in item and item.replace(".", "").isdigit():
+                        results.append((title, "REAL"))
+                    else:
+                        results.append((title, "TEXT"))
+            data.execute("INSERT INTO " + filename + "(" + insertHeader[] + ")" + "VALUES (" + values + ")")
 
     def import_json(self, filename):
         print("THIS IS A JSON")
